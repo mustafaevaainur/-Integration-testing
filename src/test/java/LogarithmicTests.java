83522528e0ee0718749b96013dc1539bbfed28a4
Ainur <79215797496@yandex.ru>
2018-04-12 16:03:11 +0300

    small refactors

--- src/test/java/LogarithmicTests.java
+++ src/test/java/LogarithmicTests.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import org.junit.Assert;
 import ru.ifmo.interfaces.ILogBaseFive;
 import ru.ifmo.interfaces.ILogBaseTwo;
 import ru.ifmo.interfaces.INaturalLog;
@@ -8,23 +7,18 @@ import ru.ifmo.logarithmic.LogBaseTwo;
 import ru.ifmo.logarithmic.NaturalLog;
 import ru.ifmo.stub.LogBaseFiveStub;
 import ru.ifmo.stub.LogBaseTwoStub;
-import ru.ifmo.stub.NaturalLogStub;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 
 public class LogarithmicTests {
-    //0.5, 1 , 2, 5
-    final double EPS = 0.01;
-    INaturalLog log = new NaturalLog(EPS);
+    final double EPS = 0.001;
+    /*INaturalLog log = new NaturalLog(EPS);
     ILogBaseTwo log2 = new LogBaseTwo(log);
-    ILogBaseFive log5 = new LogBaseFive(log);
-    /*INaturalLog ln = new NaturalLog(EPS);
+    ILogBaseFive log5 = new LogBaseFive(log);*/
     ILogBaseTwo log2 = new LogBaseTwoStub();
     ILogBaseFive log5 = new LogBaseFiveStub();
-    INaturalLog log = new NaturalLogStub();*/
 
     @Test
     public void Log2Test05() {
@@ -72,30 +66,6 @@ public class LogarithmicTests {
     @Test
     public void Log5Test5() {
         BigDecimal sub = BigDecimal.valueOf(Math.log(5) / Math.log(5)).subtract(log5.calculate(5));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    public void LogTest05() {
-        BigDecimal sub = BigDecimal.valueOf(Math.log(0.5)).subtract(log.calculate(0.5));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    public void LogTest1() {
-        BigDecimal sub = BigDecimal.valueOf(Math.log(1)).subtract(log.calculate(1));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    public void LogTest2() {
-        BigDecimal sub = BigDecimal.valueOf(Math.log(2)).subtract(log.calculate(2));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    public void LogTest5() {
-        BigDecimal sub = BigDecimal.valueOf(Math.log(5)).subtract(log.calculate(5));
         assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
     }
 
--- src/test/java/TrigonometricTests.java
+++ src/test/java/TrigonometricTests.java
@@ -1,71 +1,23 @@
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import ru.ifmo.interfaces.*;
-import ru.ifmo.logarithmic.NaturalLog;
 import ru.ifmo.stub.*;
 import ru.ifmo.trigonometric.*;
-
 import java.math.BigDecimal;
-
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 class TrigonometricTests {
-    private final double EPS = 0.1;
+    private final double EPS = 0.001;
 
-   /* private ISine sin = new Sine(EPS);
+    private ISine sin = new Sine(EPS);
     private ICosine cos = new Cosine(sin);
     private ITangent tan = new Tangent(sin);
     private ICotangent cotangent = new Cotangent(sin);
-    private ISecant sec = new Secant(sin);*/
-    private ISine sin = new SineStub();
+    private ISecant sec = new Secant(sin);
+    /*private ISine sin = new SineStub();
     private ICosine cos = new CosineStub(sin);
     private ITangent tan = new TangentStub(sin);
     private ICotangent cotangent = new CotangentStub(sin);
-    private ISecant sec = new SecantStub(sin);
-
-    /*@Test
-    void TestCosinePI() {
-        BigDecimal sub = BigDecimal.valueOf(Math.cos(Math.PI)).subtract(cos.calculate(Math.PI));
-        System.out.println("cospi "+sub);
-        assertTrue(sub.abs().compareTo(BigDecimal.valueOf(EPS)) < 0);
-    }*/
-
-    @Test
-    void TestSine0() {
-        BigDecimal sub = BigDecimal.valueOf(Math.sin(0)).subtract(sin.calculate(0));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    void TestSinePI() {
-        BigDecimal sub = BigDecimal.valueOf(Math.sin(Math.PI)).subtract(sin.calculate(Math.PI));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    void TestSinePIdiv() {
-        BigDecimal sub = BigDecimal.valueOf(Math.sin(Math.PI/2)).subtract(sin.calculate(Math.PI/2));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-    @Test
-    void TestSinePI6() {
-        BigDecimal sub = BigDecimal.valueOf(Math.sin(Math.PI/6)).subtract(sin.calculate(Math.PI/6));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    void TestSineminPI3() {
-        BigDecimal sub = BigDecimal.valueOf(Math.sin(-Math.PI/3)).subtract(sin.calculate(-Math.PI/3));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
-    @Test
-    void TestSinePI4() {
-        BigDecimal sub = BigDecimal.valueOf(Math.sin(Math.PI/4)).subtract(sin.calculate(Math.PI/4));
-        assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-    }
-
+    private ISecant sec = new SecantStub(sin);*/
 
     @Test
     void TestCosine0() {
@@ -144,23 +96,6 @@ class TrigonometricTests {
                 subtract(sec.calculate(0));
         assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
     }
-
-    /*@Test
-    void TestSecantPI() {
-        BigDecimal sub = BigDecimal.valueOf(1 / Math.cos(Math.PI)).
-                subtract(sec.calculate(Math.PI));
-        System.out.println("secpi "+sub);
-        assertTrue(sub.abs().compareTo(BigDecimal.valueOf(EPS)) < 0);
-    }
-
-    @Test
-    void TestSecantMinPI() {
-
-        BigDecimal sub = BigDecimal.valueOf(1 / Math.cos(-Math.PI)).
-                subtract(sec.calculate(-Math.PI));
-        System.out.println("secminpi "+sub);
-        assertTrue(sub.abs().compareTo(BigDecimal.valueOf(EPS)) < 0);
-    }*/
 
     @Test
     void TestSecantPIdiv4() {
