c3b48572b714ad67233c983f098769df79e9ef60
Evgeniy <Evgeniy>
2018-04-13 12:36:57 +0300

    add drawing

--- src/test/java/CompoundedFunctionTest.java
+++ src/test/java/CompoundedFunctionTest.java
@@ -1,6 +1,8 @@
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import ru.ifmo.builder.CSVCreator;
 import ru.ifmo.interfaces.*;
 import ru.ifmo.logarithmic.LogBaseFive;
 import ru.ifmo.logarithmic.LogBaseTwo;
@@ -24,13 +26,13 @@ public class CompoundedFunctionTest {
     INaturalLog ln = new NaturalLog(EPS);
     ILogBaseTwo flog2 = new LogBaseTwo(ln);
     ILogBaseFive flog5 = new LogBaseFive(ln);
-
     CompoundedFunction system = new CompoundedFunction(fsin, fcos, fsec, ftan, fcot, ln, flog2, flog5);
 
+    CSVCreator csvCreator = new CSVCreator(fsin, ln, system);
     ArrayList<Double> values = new ArrayList<>();
 
-    @BeforeEach
-    void init() {
+    @Test
+    public void SystemTestsFour(){
         values.add(-1.3d);
         values.add(-0.01d);
         values.add(-5d);
@@ -42,23 +44,26 @@ public class CompoundedFunctionTest {
         values.add(1.4d);
         values.add(5d);
         values.add(0.5d);
+        for(Double value: values) {
+            if(value >= 0 ){
+                double log2 = Math.log(value)/Math.log(2);
+                double log5 = Math.log(value)/Math.log(5);
+                BigDecimal mathLogResult = BigDecimal.valueOf((((((log2+log2)-log2)*log2)*log2)/log5));
+                BigDecimal sub = system.calculate(value).subtract(mathLogResult);
+                assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
+            } else {
+                double sec = 1/Math.cos(value);
+                double cot = Math.cos(value)/Math.sin(value);
+                double tan = Math.tan(value);
+                double cosn = Math.cos(value);
+                double sin = Math.sin(value);
+                BigDecimal mathTangResult = BigDecimal.valueOf(((((sec/tan/cot)*cosn)*sec)+(cosn/sin))-(sin+sin));
+                BigDecimal sub = system.calculate(value).subtract(mathTangResult);
+                assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
+            }
+        }
+        csvCreator.fillFillByCompoundFunctionValues(-10, 5, 0.1);
     }
 
-    @Test
-    public void SystemTestsFour(){
-        for(Double value: values) {
-            double sec = 1/Math.cos(value);
-            double cot = Math.cos(value)/Math.sin(value);
-            double tan = Math.tan(value);
-            double cosn = Math.cos(value);
-            double sin = Math.sin(value);
-            double log2 = Math.log(value)/Math.log(2);
-            double log5 = Math.log(value)/Math.log(5);
-            BigDecimal mathLogResult = BigDecimal.valueOf((((((log2+log2)-log2)*log2)*log2)/log5));
-            BigDecimal mathTangResult = BigDecimal.valueOf(((((sec/tan/cot)*cosn)*sec)+(cosn/sin))-(sin+sin));
-            BigDecimal sub = system.calculate(value).subtract(value >= 0 ? mathLogResult : mathTangResult);
-            assertEquals(sub.abs().compareTo(BigDecimal.valueOf(EPS)), -1);
-        }
-    }
 
 }
